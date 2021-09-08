ee4ea8180b3d76ddbd3ddf1882d057f741245ecb
Ida Darmochwał <ida.mdart@gmail.com>
2021-09-08 19:08:04 +0200

    Add Kitchen view

--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -2,10 +2,167 @@ import React from 'react';
 //import PropTypes from 'prop-types';
 import styles from './Kitchen.module.scss';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
 const Kitchen = () => {
+
+  const demoContent = [
+    {
+      id: 1,
+      name: 'Zia Giulia\'s Breakfast',
+      table: 3,
+      amount: 1,
+      params: {
+        coffee: {
+          label: 'Coffee type',
+          options:{
+            latte: 'Latte',
+          },
+        },
+      },
+      starters: [
+        'bread',
+      ],
+    },
+    {
+      id: 3,
+      name: 'Nonna Alba\'s Pizza',
+      table: 3,
+      amount: 2,
+      params: {
+        sauce: {
+          label: 'Sauce',
+          options: {
+            tomato: 'Tomato',
+          },
+        },
+        toppings: {
+          label: 'Toppings',
+          options: {
+            olives: 'Olives',
+            redPeppers: 'Red peppers',
+            greenPeppers: 'Green peppers',
+            mushrooms: 'Mushrooms',
+            basil: 'Fresh basil',
+          },
+        },
+        crust: {
+          label: 'pizza crust',
+          options: {
+            thin: 'thin',
+          },
+        },
+      },
+    },
+    {
+      id: 2,
+      name: 'Nonno Alberto\'s Salad',
+      amount: 1,
+      params: {
+        ingredients: {
+          label: 'Ingredients',
+          options: {
+            cucumber: 'Cucumber',
+            tomatoes: 'Tomatoes',
+            olives: 'Olives',
+            herbs: 'Fresh herbs',
+          },
+        },
+      },
+      starters: [],
+    },
+  ];
+
+  // eslint-disable-next-line no-unused-vars
+  const sortedContent = demoContent.sort(function (a, b) {
+    return (b.id - a.id);
+  });
+
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
   return(
     <div className={styles.component}>
-      <h2>Kitchen</h2>
+      <Typography variant="h4">
+        Kitchen
+      </Typography>
+      <List component="nav" aria-label="secondary mailbox folders">
+        {sortedContent.map(order => (
+          <div key={order.id}>
+            <Divider />
+            <Grid container direction="column" justifyContent="flex-start" alignItems="flex-start">
+              <Typography variant="h6">
+                {order.table ? `Table ${order.table}` : `Order ${order.id}`}
+              </Typography>
+              <ListItem>
+                <ListItemText
+                  primary={order.name}
+                  secondary={`Amount: ${order.amount}`}
+                />
+              </ListItem>
+              <ListItem>
+                {Object.values(order.params).map((obj, index) => {
+                  console.log(obj);
+                  return(
+                    <ListItemText key={index}
+                      primary={`${obj.label}:`}
+                      secondary={Object.values(obj.options).map((item, index) => (
+                        <span key={index}>{item}, </span>
+                      ))}
+                    >
+                    </ListItemText>
+                  );
+                })}
+              </ListItem>
+              <ListItem>
+                <ListItemText
+                  primary={(order.starters && order.starters.length > 0) ? `Starters: ${order.starters}` : ''}
+                />
+              </ListItem>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">ORDER STATUS</FormLabel>
+                <FormGroup row>
+                  <Typography component="div">
+                    <Grid component="label" container alignItems="center" spacing={2}>
+                      <Grid item>In progress</Grid>
+                      <Grid item>
+                        <FormControlLabel
+                          control={
+                            <Switch
+                              onChange={handleChange}
+                              name="checkedB"
+                              color="primary"
+                            />
+                          }
+                        />
+                      </Grid>
+                      <Grid item>Done</Grid>
+                    </Grid>
+                  </Typography>
+                </FormGroup>
+              </FormControl>
+            </Grid>
+            <Divider />
+          </div>
+        ))}
+      </List>
     </div>
   );
 };
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -3,12 +3,192 @@ import { Link } from 'react-router-dom';
 //import PropTypes from 'prop-types';
 import styles from './Tables.module.scss';
 
+import { Paper,  Button, Grid, Table, TableHead, TableCell, TableBody, TableRow } from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
 const Tables = () => {
+
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
+
+  const tableNumbers = [1, 2, 3, 4];
+
+  // eslint-disable-next-line no-unused-vars
+  const demoContent = {
+
+    reservations: [
+      {
+        id: 1,
+        date: '2021-05-20',
+        hour: '16:00',
+        table: 1,
+        duration: 3,
+        ppl: 4,
+        starters: [
+          'water',
+        ],
+      },
+      {
+        date: '2021-07-26',
+        hour: '12:00',
+        duration: 1,
+        ppl: 1,
+        starters: [],
+        phone: '',
+        address: '',
+        id: 2,
+      },
+      {
+        date: '2021-07-26',
+        hour: '12:00',
+        duration: 1,
+        ppl: 1,
+        starters: [],
+        phone: '',
+        address: '',
+        id: 3,
+      },
+      {
+        date: '2021-07-26',
+        hour: '12:00',
+        duration: 1,
+        ppl: 1,
+        starters: [],
+        phone: '',
+        address: '',
+        id: 4,
+      },
+    ],
+    events: [
+      {
+        id: 1,
+        date: '2021-05-20',
+        hour: '12:30',
+        table: 1,
+        repeat: false,
+        duration: 4,
+        ppl: 3,
+        starters: [],
+      },
+      {
+        id: 2,
+        date: '2021-05-19',
+        hour: '16:00',
+        table: 3,
+        repeat: 'daily',
+        duration: 2,
+        ppl: 3,
+        starters: [
+          'bread',
+          'lemonWater',
+        ],
+      },
+      {
+        id: 3,
+        date: '2021-05-19',
+        hour: '12:00',
+        table: 3,
+        repeat: 'daily',
+        duration: 2,
+        ppl: 3,
+        starters: [
+          'bread',
+          'lemonWater',
+        ],
+      },
+      {
+        id: 4,
+        date: '2021-05-19',
+        hour: '13:00',
+        table: 2,
+        repeat: 'daily',
+        duration: 4,
+        ppl: 3,
+        starters: [],
+      },
+      {
+        id: 5,
+        date: '2021-05-19',
+        hour: '18:00',
+        table: 2,
+        repeat: 'daily',
+        duration: 2,
+        ppl: 3,
+        starters: [
+          'lemonWater',
+        ],
+      },
+    ],
+  };
+
   return(
     <div className={styles.component}>
       <h2>Tables</h2>
-      <Link to={`${process.env.PUBLIC_URL}/tables/booking`}>Booking</Link>
-      <Link to={`${process.env.PUBLIC_URL}/tables/events`}>Events</Link>
+      <Grid
+        container
+        spacing={1}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Grid item>
+          <Button component={Link} to={`${process.env.PUBLIC_URL}/tables/booking`} variant="contained" color="primary" size="large">Booking</Button>
+        </Grid>
+        <Grid item>
+          <Button component={Link} to={`${process.env.PUBLIC_URL}/tables/events`} variant="contained" color="primary" size="large">Events</Button>
+        </Grid>
+      </Grid>
+      <Grid className={styles.component}>
+        <Paper>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justifyContent="space-around">
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="dd/MM/yyyy"
+                margin="normal"
+                id="date-picker-inline"
+                label="Date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+              <KeyboardTimePicker
+                margin="normal"
+                id="time-picker"
+                label="Time"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {tableNumbers.map((table, index) => (
+                  <TableCell key={index}>Table No. {table}</TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              
+            </TableBody>
+          </Table>
+        </Paper>
+      </Grid>
     </div>
   );
 };
